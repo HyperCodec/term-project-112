8002d3b74721ba9109b9d359f7ddd1906cc11248
Tristan Murphy <tristan.murphy@outlook.com>
2024-07-28 14:42:06 -0400

    write a (mostly) working collision detection system

--- player.py
+++ player.py
@@ -1,8 +1,11 @@
 from engine.vector import Vec2
 from engine.camera import PersistentRender
 from cmu_graphics import drawCircle
+from maze import getRowColFromCoordinate
 
 PLAYER_MOVE_SPEED = 3
+PLAYER_COLLIDER_RADIUS = 50
+PLAYER_OFFSET_FROM_WALL = 1e-8
 
 
 class Player(PersistentRender):
@@ -16,33 +19,60 @@ class Player(PersistentRender):
     def move(self, app, vec):
         vec *= PLAYER_MOVE_SPEED  # * dt
 
-        if not self.is_position_safe(app, vec):
-            return
+        self.make_move_safe(app, vec)
 
         self.pos += vec
         app.camera.follow_player(app, vec)
 
-    def is_position_safe(self, app, vec):
-        current_spot = self.pos // app.cell_size
-        next_spot = (self.pos + vec) // app.cell_size
+    def make_move_safe(self, app, vec):
+        current_row, current_col = getRowColFromCoordinate(app, self.pos)
+        next_pos = self.pos + vec
 
-        # no need to check anything else,
-        # spots are equal.
-        if current_spot == next_spot:
-            return True
+        # really spaghetti way of handling all this but it works.
+        # TODO fix collisions with corners. also breaks on the bottom border
+        # with index out of bounds for some reason.
+        if next_pos.x < PLAYER_COLLIDER_RADIUS:
+            vec.x = -self.pos.x + PLAYER_OFFSET_FROM_WALL + PLAYER_COLLIDER_RADIUS
+        elif next_pos.x >= app.cols*app.cell_size - PLAYER_COLLIDER_RADIUS:
+            vec.x = app.cols*app.cell_size - PLAYER_COLLIDER_RADIUS - \
+                PLAYER_OFFSET_FROM_WALL - self.pos.x
+        elif (next_pos.x - PLAYER_COLLIDER_RADIUS) // app.cell_size != current_col:
+            left = next_pos.x - PLAYER_COLLIDER_RADIUS
+            next_col = left // app.cell_size
 
-        # outer boundaries
-        if (next_spot.x < 0 or next_spot.y < 0 or
-                next_spot.x >= app.cols or next_spot.y >= app.rows):
-            return False
+            if not app.grid[current_row, next_col]:
+                vec.x = next_col*app.cell_size + app.cell_size + PLAYER_COLLIDER_RADIUS + \
+                    PLAYER_OFFSET_FROM_WALL - self.pos.x
+        elif (next_pos.x + PLAYER_COLLIDER_RADIUS) // app.cell_size != current_col:
+            right = next_pos.x + PLAYER_COLLIDER_RADIUS
+            next_col = right // app.cell_size
 
-        cell = app.grid[next_spot.y, next_spot.x]
+            if not app.grid[current_row, next_col]:
+                vec.x = next_col*app.cell_size - PLAYER_COLLIDER_RADIUS - \
+                    PLAYER_OFFSET_FROM_WALL - self.pos.x
 
-        return bool(cell)
+        if next_pos.y < PLAYER_COLLIDER_RADIUS:
+            vec.y = -self.pos.y + PLAYER_OFFSET_FROM_WALL + PLAYER_COLLIDER_RADIUS
+        elif next_pos.x >= app.rows*app.cell_size - PLAYER_COLLIDER_RADIUS:
+            vec.y = app.rows*app.cell_size - PLAYER_COLLIDER_RADIUS - \
+                PLAYER_OFFSET_FROM_WALL - self.pos.y
+        elif (next_pos.y - PLAYER_COLLIDER_RADIUS) // app.cell_size != current_row:
+            top = next_pos.y - PLAYER_COLLIDER_RADIUS
+            next_row = top // app.cell_size
+
+            if not app.grid[next_row, current_col]:
+                vec.y = next_row*app.cell_size + app.cell_size + PLAYER_COLLIDER_RADIUS + \
+                    PLAYER_OFFSET_FROM_WALL - self.pos.y
+        elif (next_pos.y + PLAYER_COLLIDER_RADIUS) // app.cell_size != current_row:
+            bottom = next_pos.y + PLAYER_COLLIDER_RADIUS
+            next_row = bottom // app.cell_size
+
+            if not app.grid[next_row, current_col]:
+                vec.y = next_row*app.cell_size - PLAYER_COLLIDER_RADIUS - \
+                    PLAYER_OFFSET_FROM_WALL - self.pos.y
 
     def render(self, app):
         screen_pos = app.camera.get_screen_coords(app, self.pos)
 
-        drawCircle(screen_pos.x.item(), screen_pos.y.item(), 60)
-
-        drawCircle(screen_pos.x.item(), screen_pos.y.item(), 60)
+        drawCircle(screen_pos.x.item(), screen_pos.y.item(),
+                   PLAYER_COLLIDER_RADIUS)
